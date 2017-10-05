7c8875fa950a7e35f193af1b96d4da33e3608bb6
Juan Eladio Sánchez Rosas <juaneladio.sanchez@gmail.com>
2017-10-05 18:38:36 -0500

    Original version 1.1 at Moodle Plugins
    Version 1.1 (2013092101)
    Intended for Moodle 2.1, 2.3, 2.4, 2.5, 2.6
    Release date: Friday, September 13, 2013, 2:37 AM
    Last known version maintained by Carina Martinez

---
+++ README.txt
@@ -0,0 +1,28 @@
+Xlsgd grade export
+*********************
+
+This plugin exports grades in xls file, adding: 
+1. Group column,  shows groups' name which students are enroled. In the "All participants" export (without selection of group), you'll see a row for each student-group couple, it means that for the same student could have as many rows as groups he was affiliated. 
+2. Date columns, for each grade item, shows the last modification date for this item grade
+
+
+Install
+**********
+
+Copy the plugin directory "xlsgd" into moodle\grade\export\. 
+Check admin notifications to install.
+
+
+
+Contact
+*******
+
+Carina Martinez
+https://moodle.org/user/profile.php?id=298052
+martinez.carina1@gmail.com
+
+
+License
+*******
+
+Released Under the GNU General Public Licence http://www.gnu.org/copyleft/gpl.html
---
+++ version.php
@@ -0,0 +1,25 @@
+<?php
+//
+// Xls file wiht Groups and Dates -- Moodle Gradebook Export module 
+// Derived from the xls file format provided in Moodle distribution
+// Created: 07/01/2013 Carina Martinez martinez.carina1@gmail.com
+// Provided "as is".  See text below for use of this module (at your own risk).
+//
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$plugin->version  = 2013092101;
+$plugin->requires = 2011070100;
+$plugin->release = "1.1";
+$plugin->maturity = MATURITY_STABLE;
